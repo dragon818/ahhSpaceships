eb5005c827a6b1e5ff5c9824df44b77f464977cc
dragon818 <dragon.yao1219@gmail.com>
2019-12-05 20:04:52 -0600

    optimiza images

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const {src, watch, dest, parallel} = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCss = require('gulp-clean-css');
+const imgmin = require('gulp-imagemin');
 
 
 function style () {
@@ -10,4 +11,12 @@ function style () {
   .pipe(dest('./dest/css'))
 }
 
+function img () {
+  return src('./src/images/**/*')
+  .pipe(imgmin([imgmin.jpegtran({progressive: true}, imgmin.optipng({optimizationLevel: 5}))]))
+  .pipe(dest('./dest/images'))
+}
+
+
 exports.style = style;
+exports.img = img;
