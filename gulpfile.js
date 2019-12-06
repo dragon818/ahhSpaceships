d85d774c4b537c1428a1d7440de3fe43e42d2585
dragon818 <dragon.yao1219@gmail.com>
2019-12-05 20:20:28 -0600

    transpile the js from es6 to es5

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const {src, watch, dest, parallel} = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCss = require('gulp-clean-css');
 const imgmin = require('gulp-imagemin');
+const babel = require('gulp-babel');
+
 
 
 function style () {
@@ -17,6 +19,15 @@ function img () {
   .pipe(dest('./dest/images'))
 }
 
+function js () {
+  return src(['./src/js/resources.js' ,'./src/js/app.js' ,'./src/js/engine.js'])
+  .pipe(babel({
+    presets: [['@babel/env',{modules: false}]]
+  }))
+  .pipe(dest('./dest/js'))
+}
+
 
 exports.style = style;
 exports.img = img;
+exports.js = js;
