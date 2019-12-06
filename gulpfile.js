848a829010903a95bf3c61d1e9ebc234ee4769ac
dragon818 <dragon.yao1219@gmail.com>
2019-12-05 19:57:04 -0600

    minify css

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 const {src, watch, dest, parallel} = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
+const cleanCss = require('gulp-clean-css');
+
 
 function style () {
   return src('./src/css/**/*.css')
   .pipe(autoprefixer())
+  .pipe(cleanCss())
   .pipe(dest('./dest/css'))
 }
 
